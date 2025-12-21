a3042851e200f9b4be5d6182f3d1119c1b98b975
Carlos Henrique <carlosouza.dev@gmail.com>
2025-12-21 20:46:26 -0300

    Criação das entidades CategoriaServico, CategoriaVeiculo e StatusAgendamento

---
+++ src/main/java/br/com/washii/domain/enums/CategoriaServico.java
@@ -0,0 +1,10 @@
+package br.com.washii.domain.enums;
+
+public enum CategoriaServico {
+    LAVAGEM_SIMPLES,
+    POLIMENTO,
+    HIGIENIZACAO,
+    VETRIFICACAO,
+    LAVAGEM_DETALHADA,
+    OUTROS;
+}
---
+++ src/main/java/br/com/washii/domain/enums/StatusAgendamento.java
@@ -0,0 +1,8 @@
+package br.com.washii.domain.enums;
+
+public enum StatusAgendamento {
+    CANCELADO,
+    AGENDADO,
+    CONCLUIDO,
+    NAO_COMPARECEU;
+}

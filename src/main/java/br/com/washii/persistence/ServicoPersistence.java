13c641fb362d401130b238c085a85516bee860c5
GeraldoFelix <geraldo.felix@academico.ifpb.edu.br>
2026-01-06 00:02:51 -0300

    Atualização de ServicoPersistencev2

--- src/main/java/br/com/washii/persistence/ServicoPersistence.java
+++ src/main/java/br/com/washii/persistence/ServicoPersistence.java
@@ -1,5 +1,7 @@
 package br.com.washii.persistence;
 
+import br.com.washii.domain.entities.LavaJato;
+import br.com.washii.domain.entities.Negocio;
 import br.com.washii.domain.entities.Servico;
 import br.com.washii.domain.repository.ServicoRepository;
 import br.com.washii.domain.enums.CategoriaServico;
@@ -7,6 +9,7 @@ import br.com.washii.infra.database.DatabaseConfig;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -78,13 +81,52 @@ public class ServicoPersistence implements ServicoRepository {
         }  catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     @Override
-    public Optional<Servico> buscarPorId(Long aLong) {
-        return Optional.empty();
+    public Optional<Servico> buscarPorId(Long id) {
+        String sql = """
+        SELECT s.id, s.nome, s.descricao, s.tipo, s.precobase,
+               n.id AS id_negocio,
+               n.cnpj,
+               n.razaoSocial,
+               
+        FROM servico s
+        JOIN negocio n ON s.id = n.id_negocio
+        WHERE s.id = ?
+    """;
+
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setLong(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                Servico servico = new Servico();
+                servico.setId(rs.getLong("id"));
+                servico.setNome(rs.getString("nome"));
+                servico.setDescricao(rs.getString("descricao"));
+                servico.setCategoriaServico(
+                        CategoriaServico.valueOf(rs.getString("tipo"))
+                );
+                servico.setPrecoBase(rs.getDouble("precobase"));
+
+                // pra terminar ainda
+
+//                Negocio negocio = new Negocio();
+//                negocio.setId(rs.getLong("id_negocio"));
+//                servico.setNegocio(negocio);
+
+                return Optional.of(servico);
+            }
+
+            return Optional.empty();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar serviço", e);
+        }
     }
+
 
     @Override
     public List<Servico> listarTodos() {

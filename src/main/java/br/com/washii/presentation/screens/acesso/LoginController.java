e6a49bb345097cec35ead9140c73bc7377caf9b4
Carlos Henrique <carlosouza.dev@gmail.com>
2026-01-25 12:35:39 -0300

    ajustes

--- src/main/java/br/com/washii/presentation/screens/acesso/LoginController.java
+++ src/main/java/br/com/washii/presentation/screens/acesso/LoginController.java
@@ -15,7 +15,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class LoginController extends BaseController{

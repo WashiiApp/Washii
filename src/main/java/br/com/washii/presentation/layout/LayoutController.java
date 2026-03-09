5549145bb360fe22684140b92dabe7e58f02eaf3
Carlos Henrique <carlosouza.dev@gmail.com>
2026-03-09 11:48:45 -0300

    refactor: remove comentário redundante e melhora nome de variável.

--- src/main/java/br/com/washii/presentation/layout/LayoutController.java
+++ src/main/java/br/com/washii/presentation/layout/LayoutController.java
@@ -6,8 +6,7 @@ import br.com.washii.service.AutenticacaoService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-// Classe pai dos controllers do MainLayout 
-// Agrupa metodos ou atributos comuns aos controllers do MainLayout 
+
 public abstract class LayoutController extends BaseController {
 
     private AutenticacaoService autenticacaoService;
@@ -21,7 +20,6 @@ public abstract class LayoutController extends BaseController {
 
     @FXML
     void onSair(){
-        // Realizar logout
         sceneManager.switchFullScene("/br/com/washii/view/acesso/acesso-layout.fxml");
         autenticacaoService.realizarLogout();
     }
@@ -29,8 +27,8 @@ public abstract class LayoutController extends BaseController {
     protected void setBoasVindas(){
         String nomeUser = Sessao.getInstance().getUsuarioLogado().getNome();
 
-        String pNomeUser = nomeUser.split(" ")[0];
+        String primeiroNomeUser = nomeUser.split(" ")[0];
 
-        lblBoasVindas.setText("Bem vindo, " + pNomeUser);
+        lblBoasVindas.setText("Bem vindo, " + primeiroNomeUser);
     }
 }
